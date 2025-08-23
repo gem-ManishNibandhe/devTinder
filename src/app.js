77a599a9ca661aba4abce89e7b28934c9678be70
Manish Nibandhe <manunibandhe5@gmail.com>
2025-08-23 21:57:30 +0530

    multi_route handler

--- src/app.js
+++ src/app.js
@@ -5,22 +5,26 @@ const app = express() // create new we server
 //request handler
 
 //this will only handle GET call to /user
-// app.get("/user", (req, res) => {
-//     res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
-// })
+app.get("/user", (req, res, next) => {
+    next();
+    res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
+}, (req, res) => {
+    console.log("Handling the route user 2!!")
+    res.send("2nd Response")
+})
 
 
 // query params   /user?userId=101
-app.get('/user', (req, res) => {     // check the sequencing 
-    console.log(req.query)
-    res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
-})
+// app.get('/user', (req, res) => {     // check the sequencing 
+//     console.log(req.query)
+//     res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
+// })
 
 // dynamic routes  : /user/101/username
-app.get('/user/:userId/:username', (req, res) => {
-    console.log(req.params)
-    res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
-})
+// app.get('/user/:userId/:username', (req, res) => {
+//     console.log(req.params)
+//     res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
+// })
 
 
 
