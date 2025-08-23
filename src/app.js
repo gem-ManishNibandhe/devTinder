69a46fd7c07aa583673caaef80f4bb2f8ec249ad
Manish Nibandhe <manunibandhe5@gmail.com>
2025-08-23 22:56:32 +0530

    code for error handling

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
 const express = require('express')
 
 const app = express() // create new we server 
+const { adminAuth, userAuth } = require("./middlewares/auth")
 
 //request handler
 
+//Handle Auth middleware for only GET requests GET , POST
+// app.use('/admin', adminAuth)
+
+
+//handling authorization with middleware  . Here for /admin/getAllData we do not need to use auth code as it checked above 
+
+// app.get('/admin/getAllData', (req, res) => {
+//     res.send("All data")
+// })
+
 //this will only handle GET call to /user
-app.get("/user", (req, res, next) => {
-    next();
-    res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
-}, (req, res) => {
-    console.log("Handling the route user 2!!")
-    res.send("2nd Response")
-})
+// app.get("/user", userAuth, (req, res, next) => {
+//     res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
+// }, (req, res) => {
+//     console.log("Handling the route user 2!!")   //middleware
+//     res.send("2nd Response")
+// })
+
+//use of middleware
 
 
 // query params   /user?userId=101
@@ -25,6 +37,24 @@ app.get("/user", (req, res, next) => {
 //     console.log(req.params)
 //     res.send({ firstName: 'Manish', lastName: 'Nibandhe' })
 // })
+
+app.use("/", (err, req, res, next) => {
+    if (err) {
+        //Log your errors
+        res.status(500).send("something went wrong")
+    }
+})
+
+app.get('/getUserdata', (req, res) => {
+    try {
+        throw new Error("asf")
+        res.send("User Data Sent")
+    } catch (err) {
+        res.status(500).send("Something  went wrong contact support team")
+    }
+
+
+})
 
 
 
---
+++ src/middlewares/auth.js
@@ -0,0 +1,25 @@
+const adminAuth = (req, res, next) => {
+    const token = "xyz";
+    const isAdminAuth = token === 'xyz'
+    if (!isAdminAuth) {
+        res.status(401).send("Not Authorized")
+    } else {
+        next()
+    }
+};
+
+const userAuth = (req, res, next) => {
+    const token = "xyz";
+    const isAdminAuth = token === 'xyz'
+    if (!isAdminAuth) {
+        res.status(401).send("Not Authorized")
+    } else {
+        next()
+    }
+};
+
+
+module.exports = {
+    adminAuth,
+    userAuth
+};
